912491d0eae97e16521dbdc57d32b64fb7fc4be5
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-02-17 20:44:09 +0000

    feat: cursor pagination w/ timestamp

--- packages/dataplane/src/api/profiles.ts
+++ packages/dataplane/src/api/profiles.ts
@@ -1,6 +1,6 @@
 import { zValidator } from '@hono/zod-validator';
 import { base } from '@shutters/lexicons';
-import { eq } from 'drizzle-orm';
+import { eq, gt } from 'drizzle-orm';
 import { Hono } from 'hono';
 import { z } from 'zod';
 import { getDb } from '../db';
@@ -10,11 +10,23 @@ export const createRouter = () => {
   const router = new Hono();
 
   router.get('/profiles', zValidator('query', getProfilesRequest), async c => {
-    const { cursor } = c.req.valid('query');
+    const { cursor, perPage } = c.req.valid('query');
+    console.log(cursor);
 
-    const profileSet = await getDb().select().from(profiles).all();
+    const profileSet = await getDb()
+      .select()
+      .from(profiles)
+      .where(gt(profiles.createdAt, cursor.toISOString()))
+      .limit(perPage)
+      .all();
 
-    return c.json({ profileSet, cursor });
+    return c.json({
+      perPage,
+      profiles: profileSet,
+      next: profileSet.length > 0
+        ? profileSet[profileSet.length - 1].indexedAt
+        : '1970-01-01T00:00:00Z',
+    });
   });
 
   router.get(
@@ -39,5 +51,6 @@ export const createRouter = () => {
 };
 
 const getProfilesRequest = z.object({
-  cursor: z.string().optional(),
+  cursor: z.coerce.date().default(new Date(0)),
+  perPage: z.coerce.number().min(1).max(100).default(50),
 });
